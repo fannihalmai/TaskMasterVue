ea0dcd3bfed324dfaf6cdf2c4a312719ae79d988
fannihalmai <fanni.halmai@efrei.net>
2024-04-25 12:55:35 +0200

    feat(views): signup, login and router

---
+++ src/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Login from './views/Login.vue';
+import Signup from './views/Signup.vue';
+import Dashboard from './views/Dashboard.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    component: Login
+  },
+  {
+    path: '/signup',
+    component: Signup
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard
+  },
+
+];
+
+export default new VueRouter({
+  mode: 'history', // Use HTML5 history mode
+  routes
+});
