6a5afdcaa9a622bbafe735a70e5376c1739c1a7b
fannihalmai <fanni.halmai@efrei.net>
2024-04-25 13:39:02 +0200

    feat(store): vuex

---
+++ src/store.js
@@ -0,0 +1,172 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    user: null,
+    selectedTaskListId: null,
+    tasks: [],
+    selectedTask: null,
+    taskLists: [],
+  },
+  mutations: {
+    setUser(state, user) {
+      state.user = user;
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.user = null;
+    },
+    setTaskLists(state, taskLists) {
+      state.taskLists = taskLists;
+    },
+    setSelectedTaskList(state, taskListId) {
+      state.selectedTaskListId = taskListId;
+    },
+    setTasks(state, tasks) {
+      console.log("New: ", tasks)
+      state.tasks = tasks;
+    },
+    setSelectedTask(state, task) {
+      state.selectedTask = task;
+    }
+  },   
+  actions: {
+    async login({ commit }, { email, password }) {
+      try {
+        const response = await fetch('http://localhost:3000/auth/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ email, password }),
+        });
+        console.log(response);
+        if (!response.ok) {
+          throw new Error('Login failed');
+        }
+        const resp = await response.json();
+        const access_token = resp.access_token;
+        const user= resp.user;
+        console.log('User token:', access_token)
+        commit('setUser', user);
+        commit('setTaskLists', user.taskLists);
+        localStorage.setItem('token', resp.access_token);
+      } catch (error) {
+        console.error('Login error:', error);
+        throw error;
+      }
+    },
+    async getUser({ commit }){
+      const profile = await fetch('http://localhost:3000/users/profile', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+      });
+
+      const prof = await profile.json();
+
+      commit('setTaskLists', prof.taskLists);
+      commit('setUser', prof);
+    },
+    async fetchTasksForTaskList({ commit, state }) {
+      try {
+        const taskListId = state.selectedTaskListId;
+        if (!taskListId) {
+          return; // No selected task list, do nothing
+        }
+        const response = await fetch(`http://localhost:3000/tasks/${taskListId}`,{
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+        });
+        if (!response.ok) {
+          throw new Error('Failed to fetch tasks');
+        }
+        const tasks = await response.json();
+        console.log("Got tasks back : ", tasks)
+        commit('setTasks', tasks);
+      } catch (error) {
+        console.error('Fetch tasks error', error);
+      }
+    },
+    async createTaskList({ commit, state }, title) {
+      try {
+        const response = await fetch('http://localhost:3000/users/taskLists', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+          body: JSON.stringify({ title }),
+        });
+        if (!response.ok) {
+          throw new Error('Failed to create task list');
+        }
+        const taskList = await response.json();
+        console.log("State list", state.taskLists)
+        commit('setTaskLists', [...state.taskLists, taskList]);
+      } catch (error) {
+        console.error('Create task list error:', error);
+      }
+    },
+    async createTask({ commit, state },  taskToCreate ) {
+      let taskListId = state.selectedTaskListId;
+      console.log(JSON.stringify(taskToCreate))
+      try {
+        const response = await fetch(`http://localhost:3000/tasks/${taskListId}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+          body: JSON.stringify(taskToCreate),
+        });
+        console.log(response)
+        if (!response.ok) {
+          throw new Error('Failed to create task');
+        }
+        const task = await response.json();
+        console.log("I got back : ", task)
+        commit('setTasks', [...state.tasks, task]);
+      } catch (error) {
+        console.error('Create task error:', error);
+      }
+    },
+    async deleteTaskList({ commit, state }, taskListId){
+      try {
+        await fetch(`http://localhost:3000/users/task-lists/${taskListId}`, {
+          method: 'DELETE',
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+        });
+        commit('setTaskLists', state.taskLists.filter(taskList => taskList.id !== taskListId));
+      } catch (error) {
+        console.error('Delete task error:', error);
+      }
+    },
+    async deleteTask({ commit, state }, taskId) {
+      try {
+        await fetch(`http://localhost:3000/tasks/${taskId}`, {
+          method: 'DELETE',
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+        });
+        commit('setTasks', state.tasks.filter(task => task.id !== taskId));
+      } catch (error) {
+        console.error('Delete task error:', error);
+      }
+    }
+  },
+  getters: {
+    isLoggedIn: state => !!state.user,
+  },
+});
